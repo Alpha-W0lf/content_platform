a38ab24bb8b7e2442cfebb4d877892edacb13f61
Tom Chacko <tchacko615@gmail.com>
2025-02-18 13:24:53 -0600

    Update init-databases.sh

--- config/postgres/init-databases.sh
+++ config/postgres/init-databases.sh
@@ -8,7 +8,14 @@ function create_database() {
     echo "Creating database '$database'"
     echo "POSTGRES_USER: $POSTGRES_USER"
     echo "database: $database"
-    local sql_command="CREATE DATABASE $database WITH OWNER = $POSTGRES_USER ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TEMPLATE = template0;"
+
+    # Check if the database already exists
+    if psql -l -t -A | grep -q "^$database|"; then
+        echo "Database '$database' already exists. Skipping creation."
+        return 0
+    fi
+
+    local sql_command="CREATE DATABASE $database WITH OWNER = \"$POSTGRES_USER\" ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TEMPLATE = template0;"
     echo "Debugging: Executing SQL: $sql_command"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" -c "$sql_command"
     echo "Database '$database' creation completed"
@@ -22,8 +29,10 @@ if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
     echo "Debugging: POSTGRES_DB: $POSTGRES_DB"
     echo "Debugging: POSTGRES_MULTIPLE_DATABASES: $POSTGRES_MULTIPLE_DATABASES"
 
-    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-        create_database $db
+    # Correctly split the database list by commas
+    IFS=',' read -ra db_array <<< "$POSTGRES_MULTIPLE_DATABASES"
+    for db in "${db_array[@]}"; do
+        create_database "${db// /}"  # Remove potential spaces
     done
     echo "Multiple databases created"
 fi
